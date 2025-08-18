8271a1822bbbdc48e72fe4387ef70125161253c6
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-18 22:57:02 +0300

    update

---
+++ resources.tf
@@ -0,0 +1,32 @@
+
+
+
+data "external" "subnet" {
+  program = ["/bin/bash", "-c", "docker network inspect -f '{{json .IPAM.Config}}' kind | jq .[0]"]
+  depends_on = [
+    kind_cluster.default
+  ]
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand(var.kind_cluster_config_path)
+  }
+}
+
+module "metallb" {
+  source = "./modules/metallb"
+  depends_on = [kind_cluster.default]
+  kind_cluster_config_path = var.kind_cluster_config_path
+}
+
+module "nginx" {
+  source = "./modules/nginx"
+  depends_on = [module.metallb]
+}
+
+module "automq" {
+  source = "./modules/automq"
+  depends_on = [module.nginx]
+}
+
